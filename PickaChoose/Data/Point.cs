663664b38aa064cf57a21416a9ec948941e42eeb
HoaiTran308 <tranvanhoai.tt4@gmail.com>
2021-10-21 17:30:55 +0700

    Add project files.

---
+++ PickaChoose/Data/Point.cs
@@ -0,0 +1,19 @@
+﻿namespace PickaChoose.Data
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        
+        public static Point operator + (Point a, Point b)
+        {
+            return new Point(a.X + b.X, a.Y + b.Y);
+        }
+    }
+}
